d60578ed7c8d391e47303fe45135130d77a10311
luis <contatoluisfernandogaleano@gmail.com>
2024-07-24 10:35:41 -0400

    feat: get-units-services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+  ],
 };
